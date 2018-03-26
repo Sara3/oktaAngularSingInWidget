f0a90f64238381399103ee301865e93ae51b61b8
sara daqiq <sabera.daqiq@yahoo.com>
2018-03-26 14:51:03 -0700

    deploying angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,61 @@
+// app.module.ts
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OktaAuthGuard
+} from '@okta/okta-angular';
 
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected.component';
+import { LoginComponent } from './login.component';
 
+const config = {
+  issuer: 'https://mytrainingcompany.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oa7jl8dec7JDBNPy2p6',
+  scope: "openid email profile address phone"
+
+}
+
+export function onAuthRequired({ oktaAuth, router }) {
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ OktaAuthGuard ],
+    data: {
+      onAuthRequired
+    }
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),    
+    OktaAuthModule.initAuth(config)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
